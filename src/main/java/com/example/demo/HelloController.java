f810d5290997bec09ac7a6b74171918d90d7d1e4
Amada García <a01569104@tec.mx>
2025-09-17 12:57:00 -0600

    first commit

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,7 +7,22 @@ import org.springframework.web.bind.annotation.*;
 public class HelloController {
 
     @GetMapping("/get")
-    public String hello() {
+    public String helloGet() {
         return "Hello, World!";
+    }
+
+    @PostMapping
+    public String helloPost() {
+        return "Created item";
+    }
+
+    @PutMapping("/{id}")
+    public String helloPut() {
+        return "Updated item!";
+    }
+
+    @DeleteMapping("/{id}")
+    public String helloDelete(@PathVariable String id) {
+        return "Deleted item with id: " + id;
     }
 }
